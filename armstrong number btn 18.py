d864a7428d562510e64e27770359dbcc0e3fc4cb
R-ISWARYA <43374792+R-ISWARYA@users.noreply.github.com>
2018-09-27 17:24:58 +0530

    Create armstrong number btn 18.py

---
+++ armstrong number btn 18.py
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,i,temp,num,rem;
+printf("enter two number (interval):");
+scanf("%d%d",&n1,&n2);
+printf(armstrong number between %dan %d are:",n1,n2);
+for(i=n1+1;i<n2;++i)
+{temp=i;
+num=0;
+while(temp!=0)
+{
+rem=(temp%10);
+num+=rem*rem*rem;
+temp/=10;
+}
+if(i==num)
+{
+printf("%d",i);
+}
+}
+return0;
+}
