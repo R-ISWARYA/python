e31a3723406f5db5bd1c2e5f1824d0c9c6419e60
R-ISWARYA <43374792+R-ISWARYA@users.noreply.github.com>
2018-09-27 17:08:22 +0530

    Create armstrong number 17.py

---
+++ armstrong number 17.py
@@ -0,0 +1,11 @@
+sum=0
+temp=num
+whil temp>0:
+digit=temp%10
+sum+=digit**3
+temp//=10
+if num==sum:
+print(num,"is an armstrong number")
+else:
+print(num,"is not an armstrong number")
+https://github.com/R-ISWARYA/python/blob/master/prime%20number16.py
